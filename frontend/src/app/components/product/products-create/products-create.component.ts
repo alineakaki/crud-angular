dc2a5f56468f25ed943d9a082776d3bcb225486b
Aline <alineakaki1@gmail.com>
2020-05-15 10:45:36 -0300

    Add frontend

---
+++ frontend/src/app/components/product/products-create/products-create.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../product.service';
+import { Router } from '@angular/router';
+import { Product } from '../product.model';
+
+@Component({
+  selector: 'app-products-create',
+  templateUrl: './products-create.component.html',
+  styleUrls: ['./products-create.component.css']
+})
+export class ProductsCreateComponent implements OnInit {
+
+  product: Product = {
+    name: '',
+    price: null
+  }
+
+  constructor(private productService: ProductService,
+    private router: Router) { }
+
+  ngOnInit(): void {
+   
+  }
+
+  createProduct(): void {
+    this.productService.create(this.product).subscribe(() => {
+      this.productService.showMessage('Game criado!')
+      this.router.navigate(['/products'])
+    })
+  }
+
+  cancel(): void {
+    this.router.navigate(['/products'])
+  }
+}
